a9e908ee4af5aed4945217335438ddca7dd42e95
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-18 10:09:04 +0100

    correccion error enunciado

--- COMPARALOSPROBLEMAS.py
+++ COMPARALOSPROBLEMAS.py
@@ -1,12 +1,12 @@
 from math import atan2
 from random import randint
 
-a1 = randint(0,10)
-a2 = randint(0,10)
-a3 = randint(0,10)
-b1 = randint(0,10)
-b2 = randint(0,10)
-b3 = randint(0,10)
+a1 = randint(0,100)
+a2 = randint(0,100)
+a3 = randint(0,100)
+b1 = randint(0,100)
+b2 = randint(0,100)
+b3 = randint(0,100)
 
 print("Las notas de Lucía son>>\nCLARIDAD DEL PROBLEMA = {} \nORIGINALIDAD = {} \nDIFICULTAD = {}".format(a1,a2,a3))
 print("Las notas de Carlos son>>\nCLARIDAD DEL PROBLEMA = {} \nORIGINALIDAD = {} \nDIFICULTAD = {}".format(b1,b2,b3))
