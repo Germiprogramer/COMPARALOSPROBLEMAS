2eccce9dc5ff839bdba21008c07205811824832b
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-18 09:55:36 +0100

    listas con las notas de cada uno

--- COMPARALOSPROBLEMAS.py
+++ COMPARALOSPROBLEMAS.py
@@ -10,3 +10,6 @@ b3 = randint(0,10)
 
 print("Las notas de Lucía son>>\nCLARIDAD DEL PROBLEMA = {} \nORIGINALIDAD = {} \nDIFICULTAD = {}".format(a1,a2,a3))
 print("Las notas de Carlos son>>\nCLARIDAD DEL PROBLEMA = {} \nORIGINALIDAD = {} \nDIFICULTAD = {}".format(b1,b2,b3))
+
+a = [a1, a2, a3]
+b = [a2, b2, b3]
