8ff84de6e8227c64a01fb242e062858741cef3a6
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-18 09:58:06 +0100

    funcion para comparar ambas listas

--- COMPARALOSPROBLEMAS.py
+++ COMPARALOSPROBLEMAS.py
@@ -13,3 +13,34 @@ print("Las notas de Carlos son>>\nCLARIDAD DEL PROBLEMA = {} \nORIGINALIDAD = {}
 
 a = [a1, a2, a3]
 b = [a2, b2, b3]
+
+
+def compareTriplets():
+    puntos_lucia = 0
+    puntos_carlos = 0
+    
+    if a[0] > b[0]:
+        puntos_lucia += 1
+    elif a[0] < b[0]:
+        puntos_carlos += 1
+    else:
+        pass
+
+    if a[1] > b[1]:
+        puntos_lucia += 1
+    elif a[1] < b[1]:
+        puntos_carlos += 1
+    else:
+        pass
+
+    if a[2] > b[2]:
+        puntos_lucia += 1
+    elif a[2] < b[2]:
+        puntos_carlos += 1
+    else:
+        pass
+
+    print("[{},{}]".format(puntos_lucia,puntos_carlos))
+    return
+
+compareTriplets()
